fe1c0062f08bcdddc2ebc924a5c33f0d827c3f91
Dahabu Saidi <dahabusaidi@gmail.com>
2021-03-16 16:27:30 +0300

    Project setup

---
+++ android/app/src/main/kotlin/com/example/simusolarApp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.simusolarApp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+
+          primarySwatch: Colors.green,
+
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Scaffold(
+          appBar: AppBar(title: Text('Notes')),
+          body: BodyLayout(),
+          floatingActionButton: FloatingActionButton(
+            tooltip: 'Increment',
+            onPressed: null,
+            child: Icon(Icons.add),
+          ),
+        )
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class BodyLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _myListView(context);
+  }
+}
+
+// replace this function with the code in the examples
+Widget _myListView(BuildContext context) {
+
+  final titles = [
+    'This is the list item 1', 'This is the list item 2',
+    'This is the list item 3', 'This is the list item 4',
+    'This is the list item 5', 'This is the list item 6'
+  ];
+
+  final icons = [
+    Icons.directions_bus, Icons.directions_car,
+    Icons.directions_railway, Icons.directions_run,
+    Icons.directions_subway, Icons.directions_transit
+  ];
+
+  return ListView.builder(
+    itemCount: titles.length,
+    itemBuilder: (context, index) {
+      return Card(
+        child: ListTile(
+          leading: Icon(icons[index]),
+          title: Text(titles[index]),
+        ),
+      );
+    },
+  );
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
