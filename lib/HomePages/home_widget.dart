b49e59b7f512d33fa6a556ef535230374d5f836d
Dahabu Saidi <dahabusaidi@gmail.com>
2021-06-04 23:38:42 +0300

    Add side nav bar

--- lib/HomePages/home_widget.dart
+++ lib/HomePages/home_widget.dart
@@ -1,6 +1,7 @@
 // Import material again
 import "package:flutter/material.dart";
 import "placeholder_widget.dart";
+import 'nav_drawer.dart';
 
 class Home extends StatefulWidget{
   @override
@@ -21,6 +22,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: NavDrawer(),
       appBar: AppBar(
         title: Text('Service App'),
       ),
